226811b0b5125982a202d5df077f02188a5cd512
dijair12 <dijair.ribeiro@aluno.faculdadeimpacta.com.br>
2020-10-22 22:10:04 -0300

    dois arquivos do curso de JS

---
+++ async_await.js
@@ -0,0 +1,20 @@
+const novaPromise = () => new Promise ((resolve, reject) => {
+    let valor = Math.floor(Math.random() * 10);
+    setInterval(() =>{
+        if(valor % 2 == 0){
+            resolve(`O valor ${valor} é par ...`);
+        }else{
+            reject(`O valor ${valor} é impar...`);
+        }
+    }, 2000);
+});
+
+async function executarPromise() {
+    try{
+        const response = await novaPromise();
+    }catch(erro){
+        console.log(erro);
+    }
+}
+
+executarPromise();
---
+++ objectShortSintax.js
@@ -0,0 +1,12 @@
+const nome = 'Programação em JavaScript';
+const preco = 'R$ 27,99';
+const horas = 25;
+
+const curso = {
+    nome,
+    preco,
+    carga_horaria: horas,
+    instrutor: 'Geek University',
+};
+
+console.log(curso);

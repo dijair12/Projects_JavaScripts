c7a7fc482c2eb05e831f2ee2ae25e139eeff4294
Dijair Vieira Ribeiro <dijair.ribeiro@qsaude.com.br>
2020-12-21 18:00:25 -0300

    entendo babel e webpack

---
+++ Secao11/src/main.js
@@ -0,0 +1,26 @@
+/* import { soma as vamosSomar, subtracao } from './helper';
+import dobrar from './dobrar';
+
+import quadrado, {metade, mypi} from './funcoes';
+
+
+import * as helpers from './helper';
+
+
+console.log(vamosSomar(40, 2));
+
+console.log(subtracao(82, 40));
+
+console.log(dobrar(8));
+
+
+console.log(quadrado(7));
+
+console.log(metade(6));
+
+console.log(mypi);
+
+console.log(helpers.soma(5, 7));
+console.log(helpers.subtracao(6, 8)); */
+
+document.querySelector('body').style.background = '#346723';
